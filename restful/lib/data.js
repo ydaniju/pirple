5bc1563a8f402ae24933e8f5cdf11a33409888c1
Yusuf Daniju <danijuyusuf@outlook.com>
2018-07-17 13:13:57 +0100

    CRUD operation on file system

---
+++ restful/lib/data.js
@@ -0,0 +1,92 @@
+/*
+ * Library for storing and editing data
+ * 
+ */
+
+// Dependencies
+const fs = require('fs');
+const path = require('path');
+
+// Container for module to be exported
+const lib = {};
+
+// Base directory of the data object
+lib.baseDir = path.join(__dirname, '../.data/');
+
+// Write data to a file
+lib.create = (dir, file, data, callback) => {
+  // Open file for writing
+  fs.open(lib.baseDir + dir + '/' + file + '.json', 'wx', (err, fileDescriptor) => {
+    if (!err && fileDescriptor) {
+      // Convert data to string
+      const stringData = JSON.stringify(data);
+      fs.writeFile(fileDescriptor, stringData, (err) => {
+        if (!err) {
+          fs.close(fileDescriptor, (err) => {
+            if (!err) {
+              callback(false);
+            } else {
+              callback('Error closing new file');
+            };
+          });
+        } else {
+          callback('Error wirting to new file');
+        };
+      });
+    } else {
+      callback('Could not create new file, it may already exist');
+    };
+  });
+};
+
+// Read data from a file
+lib.read = (dir, file, callback) => {
+  fs.readFile(lib.baseDir + dir + '/' + file + '.json', 'utf-8', (err, data) => {
+    callback(err, data);
+  });
+};
+
+// Update data inside a file
+
+lib.update = (dir, file, data, callback) => {
+  fs.open(lib.baseDir + dir + '/' + file + '.json', 'r+', (err, fileDescriptor) => {
+    if (!err && fileDescriptor) {
+      // Convert data to string
+      const stringData = JSON.stringify(data);
+
+      // Truncate the file
+      fs.ftruncate(fileDescriptor, (err) => {
+        if (!err) {
+          fs.writeFile(fileDescriptor, stringData, (err) => {
+            if (!err) {
+              fs.close(fileDescriptor, (err) => {
+                if (!err) {
+                  callback(false);
+                } else {
+                  callback('Error closing existing file');
+                };
+              });
+            };
+          });
+        };
+      });
+    } else {
+      callback('Could not open the file for updating, it may not exist yet');
+    };
+  });
+};
+
+// Delete a file 
+lib.delete = (dir, file, callback) => {
+  // unlink a file
+  fs.unlink(lib.baseDir + dir + '/' + file + '.json', (err) => {
+    if (!err) {
+      callback(false);
+    } else {
+      callback('Error deleting file');
+    };
+  });
+};
+
+// Export the module
+module.exports = lib;
